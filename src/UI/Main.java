ce8221357f783df16a5a23fbbcc2757cc14d1ed5
anyethegoat <132080584+anyethegoat@users.noreply.github.com>
2023-05-26 15:07:40 +0100

    Update Main.java

---
+++ src/UI/Main.java
@@ -0,0 +1,10 @@
+package UI;
+
+import UI.BankAtmUI;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAtmUI firstObj = new BankAtmUI();
+        firstObj.mainUI();
+    }
+}
